3196fc55748ac57dab16456bed0d53c859cfd2ff
Jordan Augé <jordan.auge@lip6.fr>
2012-03-25 20:45:54 +0200

    started doxygening the code

--- libparistraceroute/libparistraceroute/algorithm.h
+++ libparistraceroute/libparistraceroute/algorithm.h
@@ -1,46 +1,69 @@
 #ifndef ALGORITHM_H
 #define ALGORITHM_H
 
+/** 
+ * \file algorithm.h
+ * \brief Header file for algorithms and algorithm instances.
+ */
+
 #include <search.h>
 #include "probe.h"
 #include "event.h"
 #include "dynarray.h"
+
 /**
- *  algorithm status
+ * \enum status_t
+ * \brief Algorithm status constants
  */
 typedef enum {
-    STARTED,
-    STOPPED,
-    ERROR
+    STARTED,    /*!< Algorithm is started */
+    STOPPED,    /*!< Algorithm is stopped */
+    ERROR       /*!< An error occurred */
 } status_t;
 
 struct algorithm_s;
 
+/**
+ * \enum caller_type_t
+ * \brief Type of caller
+ */
 typedef enum {
-    CALLER_ALGORITHM,
+    CALLER_ALGORITHM,   /*!< caller is an algorithm */
 } caller_type_t;
 
+/**
+ * \struct caller_t
+ * \brief Structure describing the caller for an entity.
+ */
 typedef struct {
-    caller_type_t type;
+    caller_type_t type; /*!< Type of the entity that called the algorithm instance */
     union {
-        struct algorithm_instance_s *caller_algorithm;
+        struct algorithm_instance_s *caller_algorithm;  /*!< Pointer to the caller algorithm */
     };
 } caller_t;
 
+/**
+ * \struct algorithm_instance_t
+ * \brief Structure describing a running instance of an algorithm.
+ */
 typedef struct algorithm_instance_s {
-    unsigned int id;
-    struct algorithm_s *algorithm;
-    void *options;
-    probe_t *probe_skel;
-    void *data;
-    dynarray_t *events;
-    caller_t *caller;
-    struct pt_loop_s *loop;
+    unsigned int id;                /*!< Unique identifier */
+    struct algorithm_s *algorithm;  /*!< Pointer to the type of algorithm */
+    void *options;                  /*!< Pointer to an option structure specific to the algorithm */
+    probe_t *probe_skel;            /*!< Skeleton for probes forged by this algorithm instance */
+    void *data;                     /*!< Algorithm-defined data */
+    dynarray_t *events;             /*!< An array of events received by the algorithm */
+    caller_t *caller;               /*!< Reference to the entity that called the algorithm */
+    struct pt_loop_s *loop;         /*!< Pointer to a library context */
 } algorithm_instance_t;
 
+/**
+ * \struct algorithm_t
+ * \brief Structure representing an algorithm
+ */
 typedef struct algorithm_s {
-    char* name;
-    void (*handler)(struct pt_loop_s *loop, algorithm_instance_t *instance);//, void *options, probe_t *probe_skel, void **data, event_t **events, unsigned int num_events);
+    char* name;                                                                 /*!< Algorithm name */
+    void (*handler)(struct pt_loop_s *loop, algorithm_instance_t *instance);    /*!< Main handler function */
 } algorithm_t;
 
 
@@ -48,7 +71,17 @@ typedef struct algorithm_s {
  * algorithm_t
  ******************************************************************************/
 
+/**
+ * \brief Search for available algorithms by name.
+ * \param name Name of the algorithm
+ * \return An algorithm_t structure
+ */
 algorithm_t* algorithm_search(char *name);
+
+/**
+ * \brief Register an algorithm to be used by the library.
+ * \param algorithm Pointer to a structure representing the algorithm
+ */
 void algorithm_register(algorithm_t *algorithm);
 
 #define ALGORITHM_REGISTER(MOD)	\
@@ -61,9 +94,32 @@ static void __init_ ## MOD (void) {	\
  * algorithm_instance_t
  ******************************************************************************/
 
+/**
+ * \brief Create an instance of an algorithm
+ * \param loop A pointer to a library main loop context
+ * \param algorithm A pointer to a structure representing an algorithm
+ * \param options A set of algorithm-specific options
+ * \param probe_skel Skeleton for probes crafted by the algorithm
+ * \return A pointer to an algorithm instance
+ */
 algorithm_instance_t* algorithm_instance_create(struct pt_loop_s *loop, algorithm_t *algorithm, void *options, probe_t *probe_skel);
+
+/**
+ * \brief Free an algorithm instance
+ * \param instance The instance to be free'd
+ */
 void algorithm_instance_free(algorithm_instance_t *instance);
 
+/**
+ * \brief Compare two instances of an algorithm
+ * \param instance1 Pointer to the first instance
+ * \param instance2 Pointer to the second instance
+ * \return Respectively -1, 0 or 1 if instance1 is lower, equal or bigger than
+ *     instance2
+ * 
+ * The comparison is done on the instance id.
+ *
+ */
 int algorithm_instance_compare(const void *instance1, const void *instance2);
 
 void* algorithm_instance_get_options(algorithm_instance_t *instance);
--- libparistraceroute/libparistraceroute/dynarray.h
+++ libparistraceroute/libparistraceroute/dynarray.h
@@ -1,17 +1,67 @@
 #ifndef DYNARRAY_H
 #define DYNARRAY_H
 
+/**
+ * \file dynarray.h
+ * \brief Header file: dynamic array structure
+ *
+ * dynarray_t manages a dynamic array of potentially infinite size, by
+ * allocating memory in a chunk-by-chunk fashion. An initial memory_size is
+ * allocated, and this size is increased by a given amount when needed.
+ *
+ */
+
+/**
+ * \struct dynarray_t
+ * \brief Structure representing a dynamic array.
+ */
 typedef struct {
-    void **elements;
-    unsigned int size;
-    unsigned int max_size;
+    void **elements;        /*!< Pointer to the array of elements */
+    unsigned int size;      /*!< Current size of the array */
+    unsigned int max_size;  /*!< Current maximum size of the array */
 } dynarray_t;
 
+/**
+ * \brief Create a dynamic array structure.
+ * \return A dynarray_t structure representing an empty dynamic array
+ */
 dynarray_t* dynarray_create(void);
+
+/**
+ * \brief Free a dynamic array structure.
+ * \param dynarray Pointer to a dynamic array structure
+ * \param element_free Pointer to a function used to free up element resources
+ *     (can be NULL)
+ */
 void dynarray_free(dynarray_t *dynarray, void (*element_free)(void *element));
+
+/**
+ * \brief Add a new element at the end of the dynamic array.
+ * \param dynarray Pointer to a dynamic array structure
+ * \param element Pointer to the element to add
+ */
 void dynarray_push_element(dynarray_t *dynarray, void *element);
+
+/**
+ * \brief Clear a dynamic array.
+ * \param dynarray Pointer to a dynamic array structure
+ * \param element_free Pointer to a function used to free up element resources
+ *     (can be NULL)
+ */
 void dynarray_clear(dynarray_t *dynarray, void (*element_free)(void *element));
+
+/**
+ * \brief Get the current size of a dynamic array.
+ * \param dynarray Pointer to a dynamic array structure
+ * \return Current size of the dynamic array
+ */
 unsigned int dynarray_get_size(dynarray_t *dynarray);
+
+/**
+ * \brief Get all the elements inside a dynamic array.
+ * \param dynarray Pointer to a dynamic array structure
+ * \return An array of pointer to the dynamic array elements
+ */
 void **dynarray_get_elements(dynarray_t *dynarray);
 
 #endif
