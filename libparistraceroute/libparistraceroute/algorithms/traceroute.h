bc708085715ab562f0681c112b854ba853cc2966
mando <marc-olivier.buob@lip6.fr>
2012-03-26 15:38:42 +0200

    .

---
+++ libparistraceroute/libparistraceroute/algorithms/traceroute.h
@@ -0,0 +1,7 @@
+#ifndef ALGORITHMS_TRACEROUTE_H
+#define ALGORITHMS_TRACEROUTE_H
+
+typedef struct {
+} traceroute_options_t;
+
+#endif
--- libparistraceroute/libparistraceroute/field.h
+++ libparistraceroute/libparistraceroute/field.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 typedef enum {
+    TYPE_INT8,
     TYPE_INT16,
     TYPE_INT32,
     TYPE_STRING
@@ -12,20 +13,25 @@ typedef enum {
 typedef struct {
     char *key;
     union {
-        void *value;
-        uint16_t int16_value;
-        uint32_t int32_value;
-        char *string_value;
+        void     * value;
+        uint8_t    int8_value;
+        uint16_t   int16_value;
+        uint32_t   int32_value;
+        char     * string_value;
     };
     fieldtype_t type;
 } field_t;
 
-field_t *field_create_int16(char *key, uint16_t value);
-field_t *field_create_int32(char *key, uint32_t value);
-field_t *field_create_string(char *key, char *value);
-void field_free(field_t *field);
+field_t * field_create_int8  (char *key, uint8_t  value);
+field_t * field_create_int16 (char *key, uint16_t value);
+field_t * field_create_int32 (char *key, uint32_t value);
+field_t * field_create_string(char *key, char * value);
 
-#define I32(x, y) field_create_int(x, y)
+void      field_free(field_t *field);
+
+#define I8(x, y)  field_create_int8(x, y)
+#define I16(x, y) field_create_int16(x, y)
+#define I32(x, y) field_create_int32(x, y)
 #define STR(x, y) field_create_string(x, y)
 
 #endif
