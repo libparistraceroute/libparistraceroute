cea4056f79796126ac2ceb08daa8449a634ad8cd
Jordan Augé <jordan.auge@lip6.fr>
2012-03-31 00:44:02 +0200

    fixed autotools-related files

---
+++ libparistraceroute/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+make clean
+rm -f config.* configure 
+rm -rf aclocal.m4 autom4te.cache
+rm -f depcomp install-sh libtool ltmain.sh missing stamp-h1
+rm -f {doc/,man/,paris-traceroute/,libparistraceroute/,}Makefile{.in,}
+rm -rf libparistraceroute/.deps
+
