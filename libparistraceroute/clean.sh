afeb29f03bf84b8190150a3c9ff4f8992e0ff147
Jordan Augé <jordan.auge@lip6.fr>
2012-03-31 01:36:38 +0200

    clean.sh: fixed make clean when no Makefile

--- libparistraceroute/clean.sh
+++ libparistraceroute/clean.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-make clean
+test -f Makefile && make clean 
 rm -f config.* configure 
 rm -rf aclocal.m4 autom4te.cache
 rm -f depcomp install-sh libtool ltmain.sh missing stamp-h1
 rm -f {doc/,man/,paris-traceroute/,libparistraceroute/,}Makefile{.in,}
 rm -rf libparistraceroute/.deps
-
